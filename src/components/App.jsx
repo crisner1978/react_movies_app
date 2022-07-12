8a9db50f31515f36e2186da3473d01a1024ff2ed
crisner1978 <crisner.prsvr@gmail.com>
2022-07-12 12:02:01 -0500

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+// import { Route, Switch } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+        <CssBaseline />
+        <h1>React Movies App - CWR</h1>
+    </div>
+  )
+}
+
+export default App
