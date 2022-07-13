fb2728275a1e41a0e20eb1a172a47159dab8ba24
crisner1978 <crisner.prsvr@gmail.com>
2022-07-12 20:02:11 -0500

    adding sidebar

--- src/components/AppProviders.jsx
+++ src/components/AppProviders.jsx
@@ -1,14 +1,18 @@
 import { CssBaseline } from "@mui/material";
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import Navbar from "./Navbar";
+import Navbar from "./Navbar/Navbar";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const AppProviders = ({ children }) => {
+  const theme = createTheme({});
   return (
     <Router>
-      <CssBaseline />
-      <Navbar />
-      {children}
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        {children}
+      </ThemeProvider>
     </Router>
   );
 };
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Drawer,
+  Button,
+  Avatar,
+  useMediaQuery,
+} from "@mui/material";
+import {
+  Menu,
+  AccountCircle,
+  Brightness7,
+  Brightness4,
+} from "@mui/icons-material";
+import useStyles from "./styles";
+import { useTheme } from "@mui/material/styles";
+import { Link } from "react-router-dom";
+import Sidebar from "../Sidebar/Sidebar";
+
+const Navbar = () => {
+  const [mobileOpen, setMobileOpen] = useState(true);
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(max-width:600px)");
+  const theme = useTheme();
+  const isAuthenticated = true;
+
+  return (
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          {isMobile && (
+            <IconButton
+              color="inherit"
+              edge="start"
+              style={{ outline: "none" }}
+              onClick={() => {}}
+              className={classes.menuButton}
+            >
+              <Menu />
+            </IconButton>
+          )}
+          <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
+            {theme.palette.mode === "dark" ? <Brightness7 /> : <Brightness4 />}
+          </IconButton>
+          {!isMobile && "Search..."}
+          <div>
+            {!isAuthenticated ? (
+              <Button color="inherit" onClick={() => {}}>
+                Login &nbsp; <AccountCircle />
+              </Button>
+            ) : (
+              <Button
+                color="inherit"
+                component={Link}
+                to={`/profile/:id`}
+                className={classes.linkButton}
+                onClick={() => {}}
+              >
+                {!isMobile && <>My Movies &nbsp;</>}
+                <Avatar
+                  style={{ width: 30, height: 30 }}
+                  alt="Profile"
+                  src="https://lh3.googleusercontent.com/ogw/ADea4I5RNNrrNRz0OIQSw9STapagrG0BNlK2JEKuSt3c=s64-c-mo"
+                />
+              </Button>
+            )}
+          </div>
+          {isMobile && "Search..."}
+        </Toolbar>
+      </AppBar>
+      <div>
+        <nav className={classes.drawer}>
+          {isMobile ? (
+            <Drawer
+              variant="temporary"
+              anchor="right"
+              open={mobileOpen}
+              classes={{ paper: classes.drawerPaper }}
+              ModalProps={{ keepMounted: true }}
+            >
+              <Sidebar setMobileOpen={setMobileOpen} />
+            </Drawer>
+          ) : (
+            <Drawer classes={{ paper: classes.drawerPaper }}>
+              <Sidebar setMobileOpen={setMobileOpen} />
+            </Drawer>
+          )}
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
